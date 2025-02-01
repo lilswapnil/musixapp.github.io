59f019a0319671d8df658f31db7ea4644a5c555a
Swapnil Satish Bhalerao <ssbhaler@syr.edu>
2025-01-31 20:10:45 -0500

    front end working

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -3,6 +3,7 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faSearch } from '@fortawesome/free-solid-svg-icons';
 
 export default function NavBar() {
+
     return (
         <>
         <nav>
